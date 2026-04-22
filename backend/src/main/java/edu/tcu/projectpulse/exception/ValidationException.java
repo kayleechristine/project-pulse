f246298e04c8166e14bef102402e4757892d4cb3
Kaylee Morris <kaylee.christine.morris@gmail.com>
2026-04-21 19:52:39 -0500

    create general exception classes & global exception handler

---
+++ backend/src/main/java/edu/tcu/projectpulse/exception/ValidationException.java
@@ -0,0 +1,8 @@
+package edu.tcu.projectpulse.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+}
